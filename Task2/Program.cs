62698b5a864761deb2bb80730a653ecd0cd521d2
Димка я <  @mail.ru>
2022-09-23 13:56:36 +0300

    Task2 Search  max min

---
+++ Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите 1-е число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+Console.WriteLine("max a , min b");
+else 
+Console.WriteLine("max b, min a");
